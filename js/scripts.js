b394851f1a5d676af9e1f58e60a9ffd78b2cd492
Brenda Kiptim <kiptim54@gmail.com>
2018-02-19 20:32:01 +0300

    add alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
-//quite frankly this is from a youtube tutorial I don'
-var inputValue="";
-var submit=document.getElementById("submit");
-var email= document.getElementById("email");
-
-submit.onekeyup= function(event){
-  inputValue=event.target.value;
-}
-
-submit.onclick= function(event){
-  console.log("inputValue");
-  alert("input");
-};
+$(document).ready(function(){
+  $("button").click(function(){
+    alert("Thank you for subscribing");
+  });
+});
