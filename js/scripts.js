49e4d610a7b83347786ccdfdc25a1c00edab0527
Brenda Kiptim <kiptim54@gmail.com>
2018-02-20 10:19:19 +0300

    add pill tab

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("#save").click(function(event){
     var email=$("#key").val();
   alert(email+ " You are now subscribed to Anita's Kitchen! ");
+  $("#save").html("Subscribed!");
   event.preventDefault();
   });
 });
