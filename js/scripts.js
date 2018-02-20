491cabe6f510e2198e48be50bdb42887e9a24582
Brenda Kiptim <kiptim54@gmail.com>
2018-02-20 06:17:46 +0300

    add email subscription alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-  $("button").click(function(){
-    alert(" Thank you for subscribing");
+  $("#save").click(function(event){
+    var email=$("#key").val();
+  alert(email+ " You are now subscribed to Anita's Kitchen! ");
+  event.preventDefault();
   });
 });
